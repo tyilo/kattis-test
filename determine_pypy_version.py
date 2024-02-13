d8cd265fbde2215dba03c9c0a13a7d390ae9015c
Asger Hautop Drewsen <asger@tyilo.com>
2024-02-13 15:51:10 +0100

    Add helper script

---
+++ determine_pypy_version.py
@@ -0,0 +1,13 @@
+# Submit to https://open.kattis.com/problems/hello
+import re
+import sys
+
+m = re.search(r"PyPy (\S+)", sys.version)
+if not m:
+    exit(1)
+
+version = m.group(1)
+if version == "7.3.9":
+    print("Hello World!")
+else:
+    print(":(")
